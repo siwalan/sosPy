3d5251289dba24f928376530ab5c9a92108b68a6
siwalan <59278902+siwalan@users.noreply.github.com>
2021-09-06 18:03:15 +0800

    Update main.py
    Fixed copy error

--- main.py
+++ main.py
@@ -13,17 +13,15 @@
 import numpy as np
 import random
 
-from numpy.core.fromnumeric import size
-
 def objFun(x):
     result = 10*2 + (x[0]**2-10*np.cos(2*np.pi*x[0]))+ (x[1]**2-10*np.cos(2*np.pi*x[1]))
-    return np.array(result,dtype=np.float32)
+    return result
 
-maxFE = 500000
+maxFE = 50000
 ecoSize = 100
 
-ub = np.array([5.12,5.12])
-lb = np.array([-5.12,-5.12])
+ub = np.array([5.12,5.12],dtype=np.float32)
+lb = np.array([-5.12,-5.12],dtype=np.float32)
 nvar = len(ub)
 
 ecosystem = np.random.uniform(lb,ub, [ecoSize,nvar])
@@ -48,8 +46,10 @@ while (FE < maxFE):
 
         ecoNew1 = ecosystem[index,:] + np.multiply(np.random.uniform(size=[1,nvar]),(best_organism - (BF1 * mutualVector)))
         ecoNew2 = partner  + np.multiply(np.random.uniform(size=[1,nvar]),(best_organism - (BF2 * mutualVector)))
+        
         ecoNew1 = np.clip(ecoNew1, lb, ub)[0]
         ecoNew2 = np.clip(ecoNew2, lb, ub)[0]
+        
         fitnessNew1 = objFun(ecoNew1)
         fitnessNew2 = objFun(ecoNew2)
 
@@ -60,6 +60,9 @@ while (FE < maxFE):
         if fitnessNew2 < fitness[partner_idx]:
             fitness[partner_idx] = fitnessNew2
             ecosystem[partner_idx,:] = ecoNew2
+
+        if (False == (fitness == (np.apply_along_axis(objFun, axis=1, arr=ecosystem)))).any():
+            print("Error!")
 
         FE = FE + 2
 
@@ -75,22 +78,33 @@ while (FE < maxFE):
         if fitnessNew1 < fitness[index]:
             fitness[index] = fitnessNew1
             ecosystem[index,:] = ecoNew1
+        
+        if (False == (fitness == (np.apply_along_axis(objFun, axis=1, arr=ecosystem)))).any():
+            print("Error!")
+
 
         FE = FE + 1
 
         ## Parasitsm Phase
         partner_idx = random.sample(set(range(0,ecoSize))-set([index]),1)
         partner = ecosystem[partner_idx,:]
+        if (False == (fitness == (np.apply_along_axis(objFun, axis=1, arr=ecosystem)))).any():
+            print("Error!")
 
-        parasite = ecosystem[index,:]
+        parasite = ecosystem[index,:].copy()
         parasite_dim = random.sample(list(range(0,nvar)),max(int(np.random.uniform(0,nvar)),1))
         parasite[parasite_dim] = np.random.uniform(lb[parasite_dim],ub[parasite_dim], len(parasite_dim))
         fitnessParasite = objFun(parasite)
+        if (False == (fitness == (np.apply_along_axis(objFun, axis=1, arr=ecosystem)))).any():
+            print("Error!")
 
         if fitnessParasite < fitness[partner_idx]:
             fitness[partner_idx] = fitnessParasite
             ecosystem[partner_idx,:] = parasite
         
+        if (False == (fitness == (np.apply_along_axis(objFun, axis=1, arr=ecosystem)))).any():
+            print("Error!")
+
         FE = FE + 1 
 
 best_fitness, best_fitness_id = np.amin(fitness), np.argmin(fitness)
